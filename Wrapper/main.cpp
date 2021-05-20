1329e88d852c2005252fc3f7b11c03a4bc582f52
dritekop <dritekop@gmail.com>
2021-05-20 16:31:05 +0300

    add macros

--- Wrapper/main.cpp
+++ Wrapper/main.cpp
@@ -1,12 +1,14 @@
 #include "./headers/Wrapper.hpp"
 #include <iostream>
 
+#define TEN_SECONDS 10000
+
 int main() {
     wrapper::Wrapper test;
     std::time_t t = std::time(nullptr);
     std::cout
     << std::asctime(std::localtime(&t));
-    test.sleep(10000);
+    test.sleep(TEN_SECONDS);
     t = std::time(nullptr);
     std::cout
     << std::asctime(std::localtime(&t)); 
