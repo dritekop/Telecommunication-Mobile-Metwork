be114122bea1b76bc96d4444a75c57af81cb8128
dritekop <dritekop@gmail.com>
2021-06-02 10:21:12 +0300

    add whitespace to name regex to have the ability type name and surname

--- actions_parser.cpp
+++ actions_parser.cpp
@@ -19,20 +19,20 @@ int main(int argc, char** argv)
 
 void registration(std::string& act) {
     std::string number = act.substr(9);
-    std::cout << number << std::endl; 
+    std::cout << number << std::endl;
     std::cout << "Here I should initialize the subscriber instance in the userlist.\n";
 }
 
 void calling(std::string& act) {
     std::string incoming_number = act.substr(5);
-    std::cout << incoming_number << std::endl;  
-    std::cout << "Here I should initialize the changes of other subcriber.\n";  
+    std::cout << incoming_number << std::endl;
+    std::cout << "Here I should initialize the changes of other subcriber.\n";
 }
 
 void change_name(std::string& act) {
     std::string name = act.substr(5);
     std::cout << name << std::endl;
-    std::cout << "Here I should set the name of the user.\n"; 
+    std::cout << "Here I should set the name of the user.\n";
 }
 
 void action_detect(std::string& act) {
@@ -40,7 +40,7 @@ void action_detect(std::string& act) {
         std::regex::icase);
     std::regex call_regex("[c][a][l][l][ ][+][3][8][0]([0-9]{9})",\
         std::regex::icase);
-    std::regex name_regex("[n][a][m][e][ ][a-zA-Z]([a-zA-Z0-9]+)",\
+    std::regex name_regex("[n][a][m][e][ ][a-zA-Z]([ a-zA-Z0-9]+)",\
         std::regex::icase);
     std::regex unregister_regex("[u][n][r][e][g][i][s][t][e][r]",\
         std::regex::icase);
